d29131ca1a4198f9413c2c23ff6b8809da0dd0df
Viktor <victorcatanescu@yahoo.com>
2025-11-26 11:08:31 +0000

    Al doilea commit

--- verifica.sh
+++ verifica.sh
@@ -1,8 +1,14 @@
-
-
+if [ -z "$1" ]; then
+	echo "Usage: $0 <hosts-files>"
+	exit 1
+fi
+if [ ! -f "$1" ]; then
+	echo "Error: file '$1' does not exist"
+	exit 1
+fi
 while read  IP NAME; do
 	REAL=$(nslookup "$NAME" 2>/dev/null | grep "Address:" | tail -1 | cut -d' ' -f2)
 	if [ "$REAL" != "$IP" ]; then
-		echo "$NAME in \$HOME/etc/hosts !"
+		echo "$NAME in $1!"
 	fi
-done < "$HOME/etc/hosts"
+done < "$1"
